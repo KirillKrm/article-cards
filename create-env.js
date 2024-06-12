4dcf6d1c80f397172619ba75160c65343eef9c7d
KirillKrm <kirillkarmazin2301@gmail.com>
2024-06-12 15:13:14 +0300

    fix: create env

--- create-env.js
+++ create-env.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
+const path = require("path");
+
+const filePath = "./src/environments/environment.ts";
+const directory = path.dirname(filePath);
+
+if (!fs.existsSync(directory)) {
+  fs.mkdirSync(directory, { recursive: true });
+}
+
 fs.writeFileSync(
-  "./src/environments/environment.ts",
+  filePath,
   `
     export const environment = {
       apiUrl: '${process.env.API_URL}',
